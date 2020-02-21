6ef16744e355c08e6b5a7e8bb9633909a81ac1eb
carlsbug <keonwoong.min@sjsu.edu>
2020-02-20 21:45:39 -0800

    changes

---
+++ flexy/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flexy
+//
+//  Created by Keon Min on 2/14/20.
+//  Copyright © 2020 kwmin605. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
